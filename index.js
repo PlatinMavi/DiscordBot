08813420dbd69bc26c87b4cb1ea7f98198e2e2df
Alper Gezer <a.yz07x@gmail.com>
2023-06-23 22:53:29 +0300

    js çöp

--- index.js
+++ index.js
@@ -1,40 +1,51 @@
-const Discord = require("discord.js")
-const REST = require("")
-const client = new Discord.Client()
-// const require({ REST, Routes }) from 'discord.js';
+const fs = require('node:fs');
+const path = require('node:path');
+const dotenv = require("dotenv")
+const { Client, Events,Collection, GatewayIntentBits } = require('discord.js');
+dotenv.config()
+const Token = process.env.TOKEN //geting token from .env file
 
-const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
-];
+const client = new Client({ intents: [GatewayIntentBits.Guilds] }); //defining clients or smth ??
+client.commands = new Collection();
 
-const rest = new REST({ version: '10' }).setToken("OTk2NzAxNDE1Mjg2MjQzMzM4.GJf_HX.BQcJbq33L6mJeKLn9XMUqkZHrxlrbRsQ-fPduA");
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
- try {
-  console.log('Started refreshing application (/) commands.');
-
-  rest.put(Routes.applicationCommands(996701415286243338), { body: commands });
-
-  console.log('Successfully reloaded application (/) commands.');
-} catch (error) {
-  console.error(error);
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
 }
 
-// const token = process.env.TOKEN
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
+	const command = interaction.client.commands.get(interaction.commandName);
 
-client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`)
-})
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
 
-client.on("interactionCreate", async interaction=>{
-    if (!interaction.isChatInputCommand()) return;
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
 
-    if (interaction.commandName === 'ping') {
-        await interaction.reply('Pong!');
-    }
-})
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);   //idk i just copied code
+});
 
-client.login("OTk2NzAxNDE1Mjg2MjQzMzM4.GJf_HX.BQcJbq33L6mJeKLn9XMUqkZHrxlrbRsQ-fPduA")
+client.login(Token); // we make our bot login(i guess?)
